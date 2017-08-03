34fac8f48d004045917d4a069ccea745eea8743a
betchi <shinichi.minobe@gmail.com>
2017-08-03 17:06:33 +0900

    Organize directories

--- deploy/docker/dockerhub-push.sh
+++ deploy/docker/dockerhub-push.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
-user=betchi
+if [ $# != 0 ] && [ $# != 3 ]; then
+	echo -e "\033[35mInvalid arguments. ex) ./dockerhub-push.sh swagchat api latest\033[0m"
+	exit 1
+fi
+
+user=swagchat
+image=api
 tag=latest
 if [ "$1" != "" ]; then
-	tag=$1
+	user=$1
+fi
+if [ "$2" != "" ]; then
+	image=$2
+fi
+if [ "$3" != "" ]; then
+	tag=$3
 fi
 
 echo -e "\033[36m----------> Building docker image [$user/alpine-gobuild]\033[0m"
 docker build -t $user/alpine-gobuild -f ./Dockerfile-GoBuild .
 if [ $? -gt 0 ]; then
 	echo -e "\033[35mFailed!\033[0m"
-	exit
+	exit 1
 fi
 
 echo -e "\033[36m----------> Building go binary for alpine linux [swagchat-api]\033[0m"
 docker run -i -v $GOPATH/src/github.com/fairway-corp/swagchat-api:/go/src/github.com/fairway-corp/swagchat-api -w /go/src/github.com/fairway-corp/swagchat-api $user/alpine-gobuild go build
 if [ $? -gt 0 ]; then
 	echo -e "\033[35mFailed!\033[0m"
-	exit
+	exit 1
 fi
 
 mv $GOPATH/src/github.com/fairway-corp/swagchat-api/swagchat-api swagchat-api
 
-echo -e "\033[36m----------> Building docker image [$user/swagchat-api:$tag]\033[0m"
-docker build -t $user/swagchat-api:$tag -f ./Dockerfile-Dev .
+echo -e "\033[36m----------> Building docker image [$user/$image:$tag]\033[0m"
+docker build -t $user/$image:$tag -f ./Dockerfile-Dev .
 if [ $? -gt 0 ]; then
 	echo -e "\033[35mFailed!\033[0m"
-	exit
+	exit 1
 fi
 
-rm swagchat-api
-
-echo -e "\033[36m----------> Pushing [$user/swagchat-api:$tag]\033[0m"
-docker push $user/swagchat-api:$tag
+echo -e "\033[36m----------> Pushing [$user/$image:$tag]\033[0m"
+docker push $user/$image:$tag
 if [ $? -gt 0 ]; then
 	echo -e "\033[35mFailed!\033[0m"
-	exit
+	exit 1
 fi
+
+mv swagchat-api swagchat-api_alpine_amd64
 
 echo -e "\033[36m----------> Complete!\033[0m"
 
