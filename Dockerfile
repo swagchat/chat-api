5a1630a354904bed962086806d011f55387c0a1f
betchi <shinichi.minobe@gmail.com>
2017-09-22 17:02:13 +0900

    Chane repository fairway-corp/swagchat-chat-api to swagchat/chat-api

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM alpine:3.6
 ARG API_VERSION="0.8.7"
-ARG EXEC_FILE_NAME="swagchat-api_alpine_amd64"
+ARG EXEC_FILE_NAME="swagchat-chat-api_alpine_amd64"
 RUN apk --update add tzdata curl \
   && apk --no-cache add ca-certificates && update-ca-certificates --fresh \
-  && curl -LJO https://github.com/fairway-corp/swagchat-api/releases/download/v${API_VERSION}/${EXEC_FILE_NAME} \
+  && curl -LJO https://github.com/swagchat/chat-api/releases/download/v${API_VERSION}/${EXEC_FILE_NAME} \
   && chmod 700 ${EXEC_FILE_NAME} \
-  && mv ${EXEC_FILE_NAME} /bin/swagchat-api
+  && mv ${EXEC_FILE_NAME} /bin/swagchat-chat-api
 EXPOSE 9000
-CMD ["swagchat-api"]
+CMD ["swagchat-chat-api"]
--- deploy/docker/dockerhub-push.sh
+++ deploy/docker/dockerhub-push.sh
@@ -25,14 +25,14 @@ if [ $? -gt 0 ]; then
 	exit 1
 fi
 
-echo -e "\033[36m----------> Building go binary for alpine linux [swagchat-api]\033[0m"
-docker run -i -v $GOPATH/src/github.com/fairway-corp/swagchat-api:/go/src/github.com/fairway-corp/swagchat-api -w /go/src/github.com/fairway-corp/swagchat-api $user/alpine-gobuild go build
+echo -e "\033[36m----------> Building go binary for alpine linux [swagchat-chat-api]\033[0m"
+docker run -i -v $GOPATH/src/github.com/swagchat/chat-api:/go/src/github.com/swagchat/chat-api -w /go/src/github.com/swagchat/chat-api $user/alpine-gobuild go build
 if [ $? -gt 0 ]; then
 	echo -e "\033[35mFailed!\033[0m"
 	exit 1
 fi
 
-mv $GOPATH/src/github.com/fairway-corp/swagchat-api/swagchat-api swagchat-api
+mv $GOPATH/src/github.com/swagchat/chat-api/chat-api chat-api
 
 echo -e "\033[36m----------> Building docker image [$user/$image:$tag]\033[0m"
 docker build -t $user/$image:$tag .
@@ -48,7 +48,7 @@ if [ $? -gt 0 ]; then
 	exit 1
 fi
 
-mv swagchat-api swagchat-api_alpine_amd64
+mv chat-api swagchat-chat-api_alpine_amd64
 
 echo -e "\033[36m----------> Complete!\033[0m"
 
