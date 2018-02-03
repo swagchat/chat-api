deba6e6aaa1facbbf8a74f60d2b2a81ef9db79bf
betchi <shinichi.minobe@gmail.com>
2018-02-03 13:26:09 +0900

    Rename variable

--- handlers/demohtml.go
+++ handlers/demohtml.go
@@ -8,8 +8,8 @@ import (
 	"github.com/swagchat/chat-api/utils"
 )
 
-var messengerHtmlData []byte
-var baseMessengerHtmlData = `<!DOCTYPE html>
+var messengerHTMLData []byte
+var baseMessengerHTMLData = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
@@ -17,11 +17,11 @@ var baseMessengerHtmlData = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" type="text/css" media="all">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" type="text/css" media="all">
-    <link rel="stylesheet" href="https://unpkg.com/react-swagchat/dist/react-swagchat.min.css">
+    <link rel="stylesheet" href="http://localhost:9000/static/react-swagchat.min.css">
   </head>
   <body>
     <div id="swagchat" />
-    <script src="https://unpkg.com/react-swagchat/dist/react-swagchat.min.js"></script>
+    <script src="http://localhost:9000/static/react-swagchat.min.js"></script>
     <script>
       Swag.renderMessenger({
         userId: '00581ea9-3547-4c81-930c-a3ed042e4b21',
@@ -34,20 +34,20 @@ var baseMessengerHtmlData = `<!DOCTYPE html>
   </body>
 </html>`
 
-func messengerHtmlHandler(rw http.ResponseWriter, req *http.Request) {
-	if messengerHtmlData == nil {
-		tmpExHtml := strings.Replace(baseMessengerHtmlData, "SC_REACT_RTM_PROTOCOL", os.Getenv("SC_REACT_RTM_PROTOCOL"), 1)
-		tmpExHtml = strings.Replace(tmpExHtml, "SC_REACT_RTM_HOST", os.Getenv("SC_REACT_RTM_HOST"), 1)
-		tmpExHtml = strings.Replace(tmpExHtml, "SC_REACT_RTM_PATH", os.Getenv("SC_REACT_RTM_PATH"), 1)
+func messengerHTMLHandler(rw http.ResponseWriter, req *http.Request) {
+	if messengerHTMLData == nil {
+		tmpExHTML := strings.Replace(baseMessengerHTMLData, "SC_REACT_RTM_PROTOCOL", os.Getenv("SC_REACT_RTM_PROTOCOL"), 1)
+		tmpExHTML = strings.Replace(tmpExHTML, "SC_REACT_RTM_HOST", os.Getenv("SC_REACT_RTM_HOST"), 1)
+		tmpExHTML = strings.Replace(tmpExHTML, "SC_REACT_RTM_PATH", os.Getenv("SC_REACT_RTM_PATH"), 1)
 
 		chatEndpoint := os.Getenv("SC_REACT_CHAT_ENDPOINT")
 		if chatEndpoint == "" {
 			chatEndpoint = utils.AppendStrings("/", utils.API_VERSION)
 		}
-		tmpExHtml = strings.Replace(tmpExHtml, "SC_REACT_CHAT_ENDPOINT", chatEndpoint, 1)
+		tmpExHTML = strings.Replace(tmpExHTML, "SC_REACT_CHAT_ENDPOINT", chatEndpoint, 1)
 
-		messengerHtmlData = []byte(tmpExHtml)
+		messengerHTMLData = []byte(tmpExHTML)
 	}
 
-	rw.Write(messengerHtmlData)
+	rw.Write(messengerHTMLData)
 }
