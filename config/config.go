9fc89f2b3193f25090b71142a1dcb89effa123fc
betchi <shinichi.minobe@gmail.com>
2018-09-03 19:02:31 +0900

    Move const values to const.go from config.go

--- config/config.go
+++ config/config.go
@@ -14,44 +14,6 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-const (
-	// AppName is Application name
-	AppName = "chat-api"
-	// APIVersion is API version
-	APIVersion = "0"
-	// BuildVersion is API build version
-	BuildVersion = "0.9.1"
-
-	// KeyLength is key length
-	KeyLength = 32
-	// TokenLength is token length
-	TokenLength = 32
-
-	// HeaderUserID is http header for userID
-	HeaderUserID = "X-Sub"
-	// HeaderUsername is http header for username
-	HeaderUsername = "X-Preferred-Username"
-	// HeaderWorkspace is http header for workspace
-	HeaderWorkspace = "X-Realm"
-	// HeaderClientID is http header for clientID
-	HeaderClientID = "X-ClientId"
-	// HeaderRealmRoles is http header for roles
-	HeaderRealmRoles = "X-Realm-Roles"
-	// HeaderAccountRoles is http header for account roles
-	HeaderAccountRoles = "X-Account-Roles"
-
-	CtxDsCfg ctxKey = iota
-	CtxClientID
-	CtxUserID
-	CtxWorkspace
-	CtxRoomUser
-	CtxSubscription
-	CtxTracerTransaction
-	CtxTracerSpan
-
-	RoleGeneral int32 = 1
-)
-
 var (
 	cfg         = NewConfig()
 	showVersion = false
